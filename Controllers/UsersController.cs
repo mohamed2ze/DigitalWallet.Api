bcbe0258f828aff546219ed858f25eb453c2b8d6
Mohamed Amin <mohamedpo21z@gmail.com>
2026-01-28 20:03:55 +0200

    Added Users Controller

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,34 @@
+﻿using DigitalWallet.Api.Data;
+using DigitalWallet.Api.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DigitalWallet.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public UsersController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<User>>> GetUsers()
+        {
+            return await _context.Users.ToListAsync();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<User>> CreateUser(User user)
+        {
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+
+            return Ok(user);
+        }
+    }
+}
